5612c0f96a1002a34619bfece6e0442d11a037fe
Jennika Elisson <jennika.elisson@gmail.com>
2024-01-22 13:34:15 +0100

    hard coded list items & ability to remove items

--- src/components/BucketList.tsx
+++ src/components/BucketList.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react"
+import { BucketItem } from "../models/BucketItem";
+
 export const BucketList = () => {
-    return <></>
+    const [task, setTask] = useState<BucketItem[]>([
+        new BucketItem('Vanlife in Australia', false),
+        new BucketItem('Find a way to work remotely to be able to have the freedom I want', false), 
+        new BucketItem('Visit South Amera', false)
+    ]);
+
+    const handleTick = () => {
+        
+    }
+
+    const removeBucketItem = (bucketActivity: string) => {
+        setTask(task.filter((bucketItem) => bucketItem.bucketActivity !== bucketActivity))
+    }
+    return ( 
+        <>
+        <h2>Bucket List</h2>
+        <ul>
+            {task.map((bucketItem) => {
+                return (
+                    <li>
+                        <h4>{bucketItem.bucketActivity}</h4>
+                        <p>Done?<input type="checkbox" checked={bucketItem.isDone} onChange={handleTick}/></p>
+                        <button onClick={() => removeBucketItem(bucketItem.bucketActivity)}>Remove bucket list item</button>
+                    </li>
+                )
+            })}
+            
+            
+        </ul>
+        </>
+    )
 }
