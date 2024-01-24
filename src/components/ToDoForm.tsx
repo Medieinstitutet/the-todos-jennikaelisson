3828ce7efc2c11631ae30ccb10c0206e58270a53
Jennika Elisson <jennika.elisson@gmail.com>
2024-01-24 09:35:29 +0100

    added form, input tag with onChange and button

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,3 +1,17 @@
+import { ChangeEvent, useState } from "react"
+
 export const ToDoForm = () => {
-    return <>yep</>
+    const [newItem, setNewItem] = useState("");
+    console.log(newItem)
+    return (
+        <form>
+            <input 
+                type="text" 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    setNewItem(e.target.value);
+                }} 
+                value={newItem}/>
+            <button>Add bucket list item</button>
+        </form>
+    )
 }
