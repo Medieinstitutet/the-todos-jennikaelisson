2d47a8306d80287af81d563113a0b8d733f05846
Jennika Elisson <jennika.elisson@gmail.com>
2024-01-24 16:40:50 +0100

    finished form

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,17 +1,29 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
+import { BucketItem } from "../models/BucketItem";
+
 
 export const ToDoForm = () => {
-    const [newItem, setNewItem] = useState("");
-    console.log(newItem)
+    const [newItem, setNewItem] = useState<BucketItem>(new BucketItem("", false));
+    
+    const handleNewItem = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target);
+        console.log(e.target.value);
+        setNewItem({ ...newItem, bucketActivity: e.target.value })
+    }
+
+    const handleSave = (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log("Nytt item: ", newItem);
+        
+    }
+
     return (
-        <form>
-            <input 
-                type="text" 
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                    setNewItem(e.target.value);
-                }} 
-                value={newItem}/>
+        <form onSubmit={handleSave}>
+            <input type="text" onChange={handleNewItem} value={newItem.bucketActivity}/>
             <button>Add bucket list item</button>
         </form>
     )
 }
+
+//(e: ChangeEvent<HTMLInputElement>) => {setNewItem(e.target.value);}
