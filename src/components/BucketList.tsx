327f9fca4a787971b46af784f0314769392fa00e
Jennika Elisson <jennika.elisson@gmail.com>
2024-01-25 16:09:03 +0100

    added Sort button and localStorage

--- src/components/BucketList.tsx
+++ src/components/BucketList.tsx
@@ -1,47 +1,72 @@
-import { useState } from "react"
+import { useState } from "react";
 import { BucketItem } from "../models/BucketItem";
 import { RemoveBucketItem } from "./RemoveBucketItem";
 import { ToDoForm } from "./ToDoForm";
 import { PresentTasks } from "./PresentTasks";
 
 export const BucketList = () => {
-    const [tasks, setTasks] = useState<BucketItem[]>([
-        new BucketItem('Vanlife in Australia', false),
-        new BucketItem('Find a way to work remotely to have the freedom I want', false), 
-        new BucketItem('Visit South America', false), 
-        new BucketItem('Buy land and build a tiny house', false)
-    ]);
-
-    const taskDone = (bucketActivity: string) => {
-        setTasks(tasks.map((task) => { 
-            if (task.bucketActivity == bucketActivity) {
-                return { ...task, isDone: true };
-            } else {
-                return task;
-            } 
-            })
-        );        
-    }
-
-    const removeBucketItem = (bucketActivity: string) => {
-        setTasks(tasks.filter((bucketItem) => bucketItem.bucketActivity !== bucketActivity))
-    }
-
-
-    return ( 
-        <>
-        <h2>Bucket List</h2>
-        <ToDoForm />
-        <ul>
-            {tasks.map((task) => {
-                return (
-                    <li>
-                        <PresentTasks items={task} finishedTask={taskDone} key={task.bucketActivity}/>
-                        <RemoveBucketItem RemoveItem={() => removeBucketItem(task.bucketActivity)}/>
-                    </li>
-                )
-            })}
-        </ul>
-        </>
+  const [tasks, setTasks] = useState<BucketItem[]>(
+    JSON.parse(
+      localStorage.getItem("task") ||
+        JSON.stringify([
+          new BucketItem("Vanlife in Australia", false),
+          new BucketItem("Find a way to work remotely", false),
+          new BucketItem("Visit South America", false),
+          new BucketItem("Buy land and build a tiny house", false),
+        ])
     )
-}
+  );
+
+  const addToList = (additionalListItem: string) => {
+    setTasks([...tasks, new BucketItem(additionalListItem, false)]);
+  };
+
+  const taskDone = (bucketActivity: string) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.bucketActivity == bucketActivity) {
+          return { ...task, isDone: !task.isDone };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
+  const removeBucketItem = (bucketActivity: string) => {
+    setTasks(
+      tasks.filter((bucketItem) => bucketItem.bucketActivity !== bucketActivity)
+    );
+  };
+
+  const handleSort = () => {
+    setTasks(
+      [...tasks].sort((a, b) =>
+        a.bucketActivity.localeCompare(b.bucketActivity)
+      )
+    );
+    // setTasks(tasks.slice().sort((a, b) => a.bucketActivity.localeCompare(b.bucketActivity)));
+  };
+
+  localStorage.setItem("task", JSON.stringify(tasks));
+
+  return (
+    <>
+      <h2>Bucket List</h2>
+      <ToDoForm addNewItem={addToList} />
+      <button onClick={handleSort}>Sort list</button>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li key={`${Date.now()}-${Math.random()}`}>
+              <PresentTasks items={task} finishedTask={taskDone} />
+              <RemoveBucketItem
+                RemoveItem={() => removeBucketItem(task.bucketActivity)}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
