5427022280ca302042700e130c825fe884ad0134
Jennika Elisson <jennika.elisson@gmail.com>
2024-01-25 08:21:10 +0100

    updated ToDoForm with lifting state up to add new

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,17 +1,25 @@
 import { ChangeEvent, FormEvent, useState } from "react"
-import { BucketItem } from "../models/BucketItem";
+// import { BucketItem } from "../models/BucketItem";
 
+interface IAddToListProps {
+    addNewItem: (additionalListItem: string) => void;
+}
 
-export const ToDoForm = () => {
-    const [newItem, setNewItem] = useState<BucketItem>(new BucketItem("", false));
+export const ToDoForm = (props: IAddToListProps) => {
+    const [newItem, setNewItem] = useState("");
     
-    const handleNewItem = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target);
-        console.log(e.target.value);
-        setNewItem({ ...newItem, bucketActivity: e.target.value })
+    const handleAdd = () => {
+        props.addNewItem(newItem);
+        setNewItem(""); 
     }
 
-    const handleSave = (e: FormEvent) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        //console.log(e.target);
+        //console.log(e.target.value);
+        setNewItem(e.target.value)
+    }
+
+    const handleSave = (e: FormEvent) => { // behövs denna? 
         e.preventDefault();
 
         console.log("Nytt item: ", newItem);
@@ -20,10 +28,12 @@ export const ToDoForm = () => {
 
     return (
         <form onSubmit={handleSave}>
-            <input type="text" onChange={handleNewItem} value={newItem.bucketActivity}/>
-            <button>Add bucket list item</button>
+            <input type="text" onChange={handleChange} value={newItem}/>
+            <button onClick={handleAdd}>Add bucket list item</button>
         </form>
     )
 }
 
 //(e: ChangeEvent<HTMLInputElement>) => {setNewItem(e.target.value);}
+// <BucketItem>(new BucketItem("", false))
+//  ...newItem, bucketActivity: 
